c5039e0c2aa81a4a1e52cedee4e5b79437a38981
Heber Ortiz Pereira <heberop@gmail.com>
2020-07-16 17:53:00 +0100

    added module PSEverything

--- ImportModules.ps1
+++ ImportModules.ps1
@@ -5,6 +5,10 @@ if ((Get-Module PSReadLine).Version.Major -lt 2) {
     Remove-Module PSReadLine
     Import-Module "$localModulesDirectory/PSReadLine"
 }
+if (ModuleMissing PSEverything) {
+    Install-Module PSEverything
+}
+
 Import-Module "$localModulesDirectory/posh-git/src/posh-git.psd1" # slow
 Import-Module "$localModulesDirectory/oh-my-posh/oh-my-posh.psm1" #don't import the psd1, it has an incorrect string in the version field
 Import-Module "$localModulesDirectory/PowerShellGuard/PowerShellGuard.psm1" #don't import the psd1, it has an incorrect string in the version field
@@ -17,6 +21,7 @@ if ($isWin) {
     } else {
         Set-PsFzfOption -TabExpansion
     }
+    Enable-PsFzfAliases
     Import-Module "$localModulesDirectory/git-status-cache-posh-client/GitStatusCachePoshClient.psm1"
     if (!(Test-Path "$localModulesDirectory/git-status-cache-posh-client/bin/GitStatusCache.exe")) { Update-GitStatusCache }
 }
